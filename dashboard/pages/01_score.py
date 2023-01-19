72eb0d7c8548e631b027e6d4e9018cb87b9c0727
mbarek-oubairouk <119690854+mbarek-oubairouk@users.noreply.github.com>
2023-01-19 20:07:50 +0100

    Update 01_score.py

--- dashboard/pages/01_score.py
+++ dashboard/pages/01_score.py
@@ -179,10 +179,6 @@ if auth:
     ############################ Via amazone sagemaker################################################
     if 'aws' in modele_used:
         # status du service
-        sage_client = boto3.client('sagemaker', region_name=region_name,**AWS_S3_CREDS)
-        endpoint_description = sage_client.describe_endpoint(EndpointName=app_name)
-        endpoint_status = endpoint_description["EndpointStatus"]
-        st.markdown(endpoint_status)
         status_srv = check_status(app_name,region_name)
 
         if 'InService' in status_srv:
